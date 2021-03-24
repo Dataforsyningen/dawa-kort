b980e936eacfe49fb522cdbf350eef7ce00950dc
Signe <signees96@gmail.com>
2021-03-24 10:24:48 +0100

    Phased out ticket with token in index.js

--- test/index.js
+++ test/index.js
@@ -1,7 +1,6 @@
 "use strict";
 
 var express = require('express')
-  , kf = require('kf-getticket')
   , rp= require('request-promise');
 
 var app = express();
@@ -21,30 +20,13 @@ app.get('/', function (req, res) {
   });
 });
 
-app.get('/getticket', function (req, res, next) { 
-  kf.getTicket(usr,pw).then((ticket) => {
-    res.status(200).send(ticket);
-  })
-  .catch((err) => {
-    res.status(400).send('Ukendt username og password: ' + err);
-  });
-}); 
-
-
-var usr= process.argv[2]
-  , pw= process.argv[3]
-  , port= process.argv[4];
+var port = process.argv[4];
 
 if (!port) port= 3000;
 
-kf.getTicket(usr,pw).then(ticket => {
-  var server = app.listen(port, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+var server = app.listen(port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-    console.log('URL http://%s:%s', host, port);
-  });
-})
-.catch(err => {
-  console.log("Ukendt username og password (%s)",err);
+  console.log('URL http://%s:%s', host, port);
 });
