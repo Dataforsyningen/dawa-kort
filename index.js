63a4b8453ae8016c4030652a3cc0bcaf915612f6
Kortforsyningen servicekonto <elvisflesborg@protonmail.com>
2022-01-18 11:10:38 +0100

    Trying to add Datafordeler-matrikel-map.

--- index.js
+++ index.js
@@ -1,216 +1,242 @@
 "use strict";
 
-var dawautil= require('@dawadk/dawa-util')
-  , URLSearchParams = require('@ungap/url-search-params')  
-  , dawaois= require('./dawa-ois-koder.js');
+var dawautil = require('@dawadk/dawa-util'),
+    URLSearchParams = require('@ungap/url-search-params'),
+    dawaois = require('./dawa-ois-koder.js');
 
 proj4.defs([
-  [
-    'EPSG:4326',
-    '+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees'],
-  [
-      'EPSG:25832',
-      '+proj=utm +zone=32 +ellps=GRS80 +units=m +no_defs'
-  ]
+    [
+        'EPSG:4326',
+        '+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees'
+    ],
+    [
+        'EPSG:25832',
+        '+proj=utm +zone=32 +ellps=GRS80 +units=m +no_defs'
+    ]
 ]);
 
 var maxBounds= [
-  [58.4744, 17.5575],
-  [53.015, 2.47833]
+    [58.4744, 17.5575],
+    [53.015, 2.47833]
 ];
 
 exports.maxBounds= maxBounds;
 
 exports.beregnCenter= function() {
-  var x= (maxBounds[0][0]-maxBounds[1][0])/2+maxBounds[1][0]+0.5,
-      y= (maxBounds[0][1]-maxBounds[1][1])/2+maxBounds[1][1];
-  return L.latLng(x,y);
+    var x= (maxBounds[0][0]-maxBounds[1][0])/2+maxBounds[1][0]+0.5,
+        y= (maxBounds[0][1]-maxBounds[1][1])/2+maxBounds[1][1];
+    return L.latLng(x,y);
 };
 
 var token = 'd902ac31b1c3ff2d3e7f6aa7073c6c67';
+var dafuser = 'SFOOITXITH';
+var dafpass = 'okay75..Spiiritual';
+
 
 exports.viskort = function(id,token,options) {
-	var crs = new L.Proj.CRS('EPSG:25832',
-    '+proj=utm +zone=32 +ellps=GRS80 +units=m +no_defs', 
-    {
-        resolutions: [1638.4, 819.2, 409.6, 204.8, 102.4, 51.2, 25.6, 12.8, 6.4, 3.2, 1.6, 0.8, 0.4, 0.2, 0.1]
+
+    var crs = new L.Proj.CRS('EPSG:25832',
+        '+proj=utm +zone=32 +ellps=GRS80 +units=m +no_defs', 
+        {
+            resolutions: [1638.4, 819.2, 409.6, 204.8, 102.4, 51.2, 25.6, 12.8, 6.4, 3.2, 1.6, 0.8, 0.4, 0.2, 0.1]
+        }
+    );
+
+    if (typeof options === 'undefined') {
+        options= {};
     }
-  );
+    options.crs= crs;
+    options.minZoom= 2;
+    options.maxZoom= 14;
+    options.maxBounds= maxBounds;
 
-  if (typeof options === 'undefined') {
-    options= {};
-  }
-  options.crs= crs;
-  options.minZoom= 2;
-  options.maxZoom= 14;
-  options.maxBounds= maxBounds;
+    var map = new L.Map(id, options);
 
-  var map = new L.Map(id, options);
-
-  function danKort(service,layer,styles,transparent) {
-		return L.tileLayer.wms('https://api.dataforsyningen.dk/' + service, 
-			{
-				format: 'image/png',
-				maxZoom: 14,
-				minZoom: 2,
-				token: token,
-				servicename: service,
-	  		attribution: 'Data</a> fra <a href="https://dawadocs.dataforsyningen.dk">DAWA</a> | Map data &copy;  <a href="http://sdfe.dk">SDFE</a>',
-	  		layers: layer,
-	  		styles: styles,
-	  		transparent: transparent,
-	  		tiled: false
-	 		}
- 		);
-	}
-
- 	var skaermkort= danKort('topo_skaermkort', 'dtk_skaermkort', 'default', false)
-    , skaermkortdaempet= danKort('topo_skaermkort', 'dtk_skaermkort_daempet', 'default', false)
-    //, skaermkortgraa= danKort('topo_skaermkort', 'dtk_skaermkort_graa', 'default', false)
- 		, ortofoto= danKort('orto_foraar', 'orto_foraar', 'default', false)
- 	//	, quickortofoto= danKort('orto_foraar_temp', 'quickorto_2017_10cm', 'default', false)
-    , historisk1842til1899= danKort('topo20_hoeje_maalebordsblade', 'dtk_hoeje_maalebordsblade', 'default', false)
-    , historisk1928til1940= danKort('topo20_lave_maalebordsblade', 'dtk_lave_maalebordsblade', 'default', false)
- 		, matrikelkort= danKort('mat', 'Centroide,MatrikelSkel,OptagetVej','sorte_centroider,sorte_skel,default','true')
- 		, postnrkort= danKort('dagi', 'postdistrikt', 'default','true')
- 		, kommunekort= danKort('dagi', 'kommune', 'default','true');
-
-  var adressekort = L.tileLayer.wms('https://kort.aws.dk/geoserver/aws4_wms/wms', {
-      transparent: true,
-      layers: 'adgangsadresser',
-      format: 'image/png',
-      continuousWorld: true
-    });
-  var vejpunktkort = L.tileLayer.wms('https://kort.aws.dk/geoserver/aws4_wms/wms', {
-      transparent: true,
-      layers: 'vejpunkter',
-      format: 'image/png',
-      continuousWorld: true
-    });
-  var vejpunktlinjekort = L.tileLayer.wms('https://kort.aws.dk/geoserver/aws4_wms/wms', {
-      transparent: true,
-      layers: 'vejpunktlinjer',
-      format: 'image/png',
-      continuousWorld: true
-    }); 
-  var vejnavnelinjer = L.tileLayer.wms('https://kort.aws.dk/geoserver/aws4_wms/wms', {
-      transparent: true,
-      layers: 'vejnavnelinjer',
-      format: 'image/png',
-      continuousWorld: true
-    });
-  var vejnavneomraader = L.tileLayer.wms('https://kort.aws.dk/geoserver/aws4_wms/wms', {
-      transparent: true,
-      layers: 'vejnavneomraader',
-      format: 'image/png',
-      continuousWorld: true
-    });
-  var vejtilslutningspunkter = L.tileLayer.wms('https://kort.aws.dk/geoserver/aws4_wms/wms', {
-      transparent: true,
-      layers: 'vejtilslutningspunkter',
-      format: 'image/png',
-      continuousWorld: true
-    });
-
- 	 var baselayers = {
-    "Skærmkort": skaermkort,
-    "Skærmkort - dæmpet": skaermkortdaempet,
-   // "Skærmkort - gråt": skaermkortgraa,
-    "Ortofoto": ortofoto,
-   // "Quick ortofoto": quickortofoto,
-    "Historisk 1842-1899": historisk1842til1899,
-    "Historisk 1928-1940": historisk1928til1940
-  };
-
-  var overlays = {
-   	"Matrikler": matrikelkort,
-   	"Kommuner": kommunekort,
-   	"Postnumre": postnrkort,
-    "Adresser": adressekort,
-    "Vejpunkter": vejpunktkort,
-    "Vejpunktlinjer": vejpunktlinjekort,
-    "Vejnavnelinjer": vejnavnelinjer,
-    "Vejnavneområder": vejnavneomraader,
-    "Vejtilslutningspunkter": vejtilslutningspunkter
-  };
-
-
-  if (typeof options.baselayer === 'undefined') {
-    options.baselayer= "Skærmkort";
-  }
-  baselayers[options.baselayer].addTo(map);
-
-
-  L.control.layers(baselayers, overlays, {position: 'bottomleft'}).addTo(map);
-
-  map.on('baselayerchange', function (e) {
-    if (e.name === 'Skærmkort' ||
-    		e.name === "Skærmkort - dæmpet" ||
-        e.name === "Historisk 1842-1899"||
-        e.name === "Historisk 1928-1940") {
-        matrikelkort.setParams({
-            styles: 'sorte_centroider,sorte_skel,default'
-        });
-        postnrkort.setParams({
-            styles: 'default'
-        });
-        kommunekort.setParams({
-            styles: 'default'
-        });
-    } else if (e.name === 'Flyfoto') {
-        matrikelkort.setParams({
-            styles: 'gule_centroider,gule_skel,Gul_OptagetVej,default'
-        });
-        postnrkort.setParams({
-            styles: 'yellow'
-        });
-        kommunekort.setParams({
-            styles: 'yellow'
-        });
+    function danKort(service,layer,styles,transparent) {
+        return L.tileLayer.wms('https://api.dataforsyningen.dk/' + service, 
+            {
+                format: 'image/png',
+                maxZoom: 14,
+                minZoom: 2,
+                token: token,
+                attribution: 'Data</a> fra <a href="https://dawadocs.dataforsyningen.dk">DAWA</a> | Map data &copy;  <a href="http://sdfe.dk">SDFE</a>',
+                layers: layer,
+                styles: styles,
+                transparent: transparent,
+                tiled: false
+            }
+        );
     }
-  });
 
-	map.fitBounds(maxBounds);
+    function danDAFKort(service,layer,styles,transparent) {
+        return L.tileLayer.wms('https://services.datafordeler.dk/' + service + '/1.0.0/WMS', 
 
-	return map;
+            {
+                format: 'image/png',
+                maxZoom: 14,
+                minZoom: 2,
+                username: dafuser,
+                password: dafpass,
+                attribution: 'Data</a> fra <a href="https://dawadocs.dataforsyningen.dk">DAWA</a> | Map data &copy;  <a href="http://sdfe.dk">SDFE</a>',
+                layers: layer,
+                styles: styles,
+                transparent: transparent,
+                tiled: false
+            }
+        );
+    }
+
+    var skaermkort = danKort('topo_skaermkort', 'dtk_skaermkort', 'default', false),
+        skaermkortdaempet = danKort('topo_skaermkort', 'dtk_skaermkort_daempet', 'default', false),
+        ortofoto = danKort('orto_foraar_DAF', 'orto_foraar', 'default', false),
+        historisk1842til1899 = danKort('topo20_hoeje_maalebordsblade_DAF', 'dtk_hoeje_maalebordsblade', 'default', false),
+        historisk1928til1940 = danKort('topo20_lave_maalebordsblade_DAF', 'dtk_lave_maalebordsblade', 'default', false),
+        postnrkort = danKort('dagi', 'postdistrikt', 'default','true'),
+        kommunekort = danKort('dagi', 'kommune', 'default','true');
+
+
+    var matrikelkort = danDAFKort('Matrikel/MatrikelGaeldendeOgForeloebigWMS', 'Centroide_Gaeldende,MatrikelSkel_Gaeldende,OptagetVej_Gaeldende','Sorte_centroider,Sorte_skel,default','TRUE');
+
+    var adressekort = L.tileLayer.wms('https://kort.aws.dk/geoserver/aws4_wms/wms', {
+        transparent: true,
+        layers: 'adgangsadresser',
+        format: 'image/png',
+        continuousWorld: true
+        });
+
+    var vejpunktkort = L.tileLayer.wms('https://kort.aws.dk/geoserver/aws4_wms/wms', {
+        transparent: true,
+        layers: 'vejpunkter',
+        format: 'image/png',
+        continuousWorld: true
+        });
+
+    var vejpunktlinjekort = L.tileLayer.wms('https://kort.aws.dk/geoserver/aws4_wms/wms', {
+        transparent: true,
+        layers: 'vejpunktlinjer',
+        format: 'image/png',
+        continuousWorld: true
+        }); 
+
+    var vejnavnelinjer = L.tileLayer.wms('https://kort.aws.dk/geoserver/aws4_wms/wms', {
+        transparent: true,
+        layers: 'vejnavnelinjer',
+        format: 'image/png',
+        continuousWorld: true
+        });
+
+    var vejnavneomraader = L.tileLayer.wms('https://kort.aws.dk/geoserver/aws4_wms/wms', {
+        transparent: true,
+        layers: 'vejnavneomraader',
+        format: 'image/png',
+        continuousWorld: true
+        });
+
+    var vejtilslutningspunkter = L.tileLayer.wms('https://kort.aws.dk/geoserver/aws4_wms/wms', {
+        transparent: true,
+        layers: 'vejtilslutningspunkter',
+        format: 'image/png',
+        continuousWorld: true
+        });
+
+    var baselayers = {
+        "Skærmkort": skaermkort,
+        "Skærmkort - dæmpet": skaermkortdaempet,
+        "Ortofoto": ortofoto,
+        "Historisk 1842-1899": historisk1842til1899,
+        "Historisk 1928-1940": historisk1928til1940
+    };
+
+    var overlays = {
+      "Matrikler": matrikelkort,
+      "Kommuner": kommunekort,
+      "Postnumre": postnrkort,
+      "Adresser": adressekort,
+      "Vejpunkter": vejpunktkort,
+      "Vejpunktlinjer": vejpunktlinjekort,
+      "Vejnavnelinjer": vejnavnelinjer,
+      "Vejnavneområder": vejnavneomraader,
+      "Vejtilslutningspunkter": vejtilslutningspunkter
+    };
+
+
+    if (typeof options.baselayer === 'undefined') {
+      options.baselayer= "Skærmkort";
+    }
+    baselayers[options.baselayer].addTo(map);
+
+
+    L.control.layers(baselayers, overlays, {position: 'bottomleft'}).addTo(map);
+
+    map.on('baselayerchange', function (e) {
+        if (e.name === 'Skærmkort' ||
+            e.name === "Skærmkort - dæmpet" ||
+            e.name === "Historisk 1842-1899"||
+            e.name === "Historisk 1928-1940") {
+
+            matrikelkort.setParams({
+                styles: 'sorte_centroider,sorte_skel,default'
+            });
+            postnrkort.setParams({
+                styles: 'default'
+            });
+            kommunekort.setParams({
+                styles: 'default'
+            });
+        } else if (e.name === 'Flyfoto') {
+            matrikelkort.setParams({
+                styles: 'gule_centroider,gule_skel,Gul_OptagetVej,default'
+            });
+            postnrkort.setParams({
+                styles: 'yellow'
+            });
+            kommunekort.setParams({
+                styles: 'yellow'
+            });
+        }
+    });
+
+    map.fitBounds(maxBounds);
+
+    return map;
 };
 
 exports.etrs89towgs84= function(x,y) {
-	  return proj4('EPSG:25832','EPSG:4326', {x:x, y:y});  
+    return proj4('EPSG:25832','EPSG:4326', {x:x, y:y});  
 };
 
 exports.geojsontowgs84= function(geojson) {
-  return L.Proj.geoJson(geojson);
+    return L.Proj.geoJson(geojson);
 };
 
 
 exports.nærmesteAdgangsadresse= function(getMap) {
-  return function(e) {
-    fetch(dawautil.danUrl("https://api.dataforsyningen.dk/adgangsadresser/reverse",{x: e.latlng.lng, y: e.latlng.lat, medtagugyldige: true}))
-    .catch(function (error) {
-      alert(error.message);
-    })
-    .then(function(response) {
-      if (response.status >=400 && response.status <= 499) {
-        response.json().then(function (object) {
-          alert(object.type + ': ' + object.title);
-        });
-      }
-      else if (response.status >= 200 && response.status <=299 ){
-        return response.json();
-      }
-    }) 
-    .then( function ( adgangsadresse ) { 
+    return function(e) {
+        fetch(dawautil.danUrl("https://api.dataforsyningen.dk/adgangsadresser/reverse",{x: e.latlng.lng, y: e.latlng.lat, medtagugyldige: true}))
+        .catch(function (error) {
+          alert(error.message);
+        })
+        .then(function(response) {
+            if (response.status >=400 && response.status <= 499) {
+                response.json().then(function (object) {
+                    alert(object.type + ': ' + object.title);
+                });
+            }
+            else if (response.status >= 200 && response.status <=299 ){
+                return response.json();
+            }
+        }) 
+        .then( function ( adgangsadresse ) { 
 
-      var x= adgangsadresse.adgangspunkt.koordinater[1]
-        , y= adgangsadresse.adgangspunkt.koordinater[0];
-      var marker= L.circleMarker(L.latLng(x, y), {color: 'red', fillColor: 'red', stroke: true, fillOpacity: 1.0, radius: 4, weight: 2, opacity: 1.0}).addTo(getMap());//defaultpointstyle);
-      var popup= marker.bindPopup(L.popup().setContent("<a href='https://info.dataforsyningen.dk/adgangsadresser?id="+adgangsadresse.id+"'>" + dawautil.formatAdgangsadresse(adgangsadresse) + "</a>"),{autoPan: true});
-      if (adgangsadresse.vejpunkt) {
-        var vx= adgangsadresse.vejpunkt.koordinater[1]
-          , vy= adgangsadresse.vejpunkt.koordinater[0];
-        var vpmarker= L.circleMarker(L.latLng(vx, vy), {color: 'blue', fillColor: 'blue', stroke: true, fillOpacity: 1.0, radius: 4, weight: 2, opacity: 1.0}).addTo(getMap());//defaultpointstyle);
-        vpmarker.bindPopup(L.popup().setContent("<a href='https://info.dataforsyningen.dk/adgangsadresser?id="+adgangsadresse.id+"'>" + dawautil.formatAdgangsadresse(adgangsadresse) + "</a>"),{autoPan: true});
+          var x= adgangsadresse.adgangspunkt.koordinater[1]
+            , y= adgangsadresse.adgangspunkt.koordinater[0];
+          var marker= L.circleMarker(L.latLng(x, y), {color: 'red', fillColor: 'red', stroke: true, fillOpacity: 1.0, radius: 4, weight: 2, opacity: 1.0}).addTo(getMap());//defaultpointstyle);
+          var popup= marker.bindPopup(L.popup().setContent("<a href='https://info.dataforsyningen.dk/adgangsadresser?id="+adgangsadresse.id+"'>" + dawautil.formatAdgangsadresse(adgangsadresse) + "</a>"),{autoPan: true});
+          if (adgangsadresse.vejpunkt) {
+            var vx= adgangsadresse.vejpunkt.koordinater[1]
+              , vy= adgangsadresse.vejpunkt.koordinater[0];
+            var vpmarker= L.circleMarker(L.latLng(vx, vy), {color: 'blue', fillColor: 'blue', stroke: true, fillOpacity: 1.0, radius: 4, weight: 2, opacity: 1.0}).addTo(getMap());//defaultpointstyle);
+            vpmarker.bindPopup(L.popup().setContent("<a href='https://info.dataforsyningen.dk/adgangsadresser?id="+adgangsadresse.id+"'>" + dawautil.formatAdgangsadresse(adgangsadresse) + "</a>"),{autoPan: true});
       }
 
       getMap().setView(L.latLng(x, y),12);
